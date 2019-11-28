6a7cde13978b877475cf67a10184f54d72997167
Jacek Kawalec <jokerchocolate@gmail.com>
2019-11-28 09:01:35 +0100

    Added Bonus 1 + fixed error with object creation w/o **kwargs

--- easydict.py
+++ easydict.py
@@ -1,7 +1,10 @@
 class EasyDict:
-    def __init__(self, _dict):
+    def __init__(self, _dict={}):
         self.__dict__.update(_dict)
 
     def __getitem__(self, key):
         return self.__getattribute__(key)
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
 
