ada16bb3d0d54fc906d90ed4b6df7480aba9f5f8
Jacek Kawalec <jokerchocolate@gmail.com>
2019-12-13 11:12:13 +0100

    Added Folders for each resolved problem

---
+++ uniques/uniques.py
@@ -0,0 +1,9 @@
+# def uniques_only(iter):
+#     return set(iter)
+
+# def uniques_only(iter):
+#     uniques = []
+#     for i in iter:
+#         if i not in uniques:
+#             uniques.append(i)
+#     return uniques
